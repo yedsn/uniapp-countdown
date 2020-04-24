90de7f6456bd9e0f7c62098f8f19e645e2902f1f
jam <yedsn@163.com>
2020-04-24 10:02:44 +0800

    第一个完善的版本

---
+++ README.md
@@ -0,0 +1,40 @@
+### 倒计时组件（自由定制版）
+
+#### 效果展示
+![效果展示](http://p2.so.qhmsg.com/t02ed82442262902b46.jpg)
+
+简单自由的时间倒计时组件，只封装倒计时逻辑，需要显示什么和显示的样式都由你控制。
+
+****
+
+#### 使用示例
+
+```html
+<countdown-timer :time="time">
+    <template v-slot="{day, hour, minute, second}">
+        <view>{{day}}天{{hour}}时{{minute}}分{{second}}秒</view>		
+    </template>
+</countdown-timer>
+```
+
+#### 配置
+
+- time 倒计时的毫秒数
+
+#### slot数据
+- day 天
+- hour 小时
+- minute 分钟
+- second 秒
+- remain 剩余毫秒数
+
+#### 事件
+
+  - finish 倒计时结束事件
+
+#### 组件可调用方法
+
+  - restart 重新开始倒计时
+
+
+
---
+++ components/countdown-timer/countdown-timer.vue
@@ -0,0 +1,67 @@
+<template>
+	<view><slot :timeData="timeData" :remain="timeData.remain" :day="timeData.day" :hour="timeData.hour" :minute="timeData.minute" :second="timeData.second" /></view>
+</template>
+
+<script>
+export default {
+	props: {
+		// 倒计时时长（单位：毫秒）
+		time: {
+			type: Number,
+			default: 0
+		}
+	},
+	data() {
+		return {
+			timer: null,
+			timeData: {
+				remain: 0,
+				day: 0,
+				hour: 0,
+				minute: 0,
+				second: 0
+			}
+		};
+	},
+	watch: {
+		time() {
+			this.timeData.remain = this.time;
+			this.restart();
+		}
+	},
+	methods: {
+		startTimer() {
+			this.updateTime()
+			this.timer = setInterval(() => {
+				this.updateTime()
+			}, 1000);
+		},
+		updateTime() {
+			let t = this.timeData.remain;
+			this.timeData.day = Math.floor(t / 1000 / 60 / 60 / 24);
+			this.timeData.hour = Math.floor((t / 1000 / 60 / 60) % 24);
+			this.timeData.minute = Math.floor((t / 1000 / 60) % 60);
+			this.timeData.second = Math.floor((t / 1000) % 60);
+			this.timeData.remain -= 1000;
+			if (this.timeData.remain < 0) {
+				clearInterval(this.timer);
+				this.$emit('finish');
+			}
+		},
+		restart() {
+			if (this.timer != null) {
+				clearInterval(this.timer);
+			}
+			this.timeData.remain = this.time;
+			this.startTimer();
+		}
+	},
+	mounted() {
+		this.timeData.remain = this.time;
+		this.startTimer();
+	},
+	beforeDestroy() {
+		clearInterval(this.timer);
+	}
+};
+</script>
